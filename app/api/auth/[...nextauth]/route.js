b7652c96c31162008c6831d735db5a9b0b720dd6
Owen Muchini <owenmuchini1@gmail.com>
2023-05-24 07:10:21 +0930

    Fixed the env problem, authentication works and database connects

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,40 +7,43 @@ import { connectToDB } from "@utils/database";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: "682180918225-gbbni88fla3mrvrg57bb7317et6jlt16.apps.googleusercontent.com",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            // Need to replace this with env variables or a secret holder variable
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        session.user.id = sessionUser._id.toString();
-
-        return session;
-    },
-    async signIn({ profile }){
-        try {
-            await connectToDB();
-
-            // check if a user already exists
-            const userExists = await User.findOne({
-                email: profile.email
-            });
-
-            // if not, create a new user and save it to the database
-            if(!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ","").toLowerCase(),
-                    image: profile.picture
-                })
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
+    
+            session.user.id = sessionUser._id.toString();
+    
+            return session;
+        },
+        async signIn({ profile }){
+            try {
+                await connectToDB();
+    
+                // check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+    
+                // if not, create a new user and save it to the database
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ","").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
         }
     }
 })
